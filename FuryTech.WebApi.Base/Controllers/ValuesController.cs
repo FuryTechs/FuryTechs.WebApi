9fd49ce1ce3300cb098b5f69bfe57de732083b94
carathorys <gallayb@gmail.com>
2019-05-28 10:31:13 +0200

    Initial commit

---
+++ FuryTech.WebApi.Base/Controllers/ValuesController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Threading.Tasks;
+using FuryTech.WebApi.Base.Db.Model;
+using FuryTech.WebApi.Controllers;
+using FuryTechs.BLM.NetStandard.Interfaces;
+using Microsoft.AspNetCore.Mvc;
+
+namespace FuryTech.WebApi.Base.Controllers
+{
+    public class ValuesController : BaseApiController<User, int>
+    {
+        public ValuesController(IRepository<User> repository) : base(repository)
+        {
+        }
+
+        protected override Expression<Func<User, int>> GetKeyValue() => x => x.Id;
+    }
+}
---
+++ FuryTech.WebApi.Base/Db/DatabaseContext.cs
@@ -0,0 +1,28 @@
+using System;
+using FuryTech.WebApi.Base.Db.Model;
+using Microsoft.EntityFrameworkCore;
+
+namespace FuryTech.WebApi.Base.Db
+{
+    public class DatabaseContext : DbContext
+    {
+        public DatabaseContext() : base()
+        {
+        }
+
+        public DatabaseContext(DbContextOptions options) : base(options)
+        {
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+            modelBuilder.Entity<User>().HasData(new[] {
+                new User { Id = 1, FirstName = "Melinda", LastName = "Gallayné Vrhovina", Email = "vrhovina.melinda@gmail.com" },
+                new User { Id = 2, FirstName = "Balázs", LastName = "Gallay", Email="gallayb@gmail.com"},
+            });
+        }
+
+        public DbSet<User> Users { get; set; }
+    }
+}
